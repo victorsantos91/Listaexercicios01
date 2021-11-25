092d208c36feedc4af92c802182aef2ae3b2861f
victorsantos91 <90118154+victorsantos91@users.noreply.github.com>
2021-11-25 00:43:45 -0300

    Concluído exercício 4

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("digite o preço do primeiro produto:");
+            string? textoPreço01 = Console.ReadLine();
+            Console.WriteLine("digite o preço  segundo produto:");
+            string? textoPreço02 = Console.ReadLine();
+            Console.WriteLine("digite o preço do terceiro produto :");
+            string? textoPreço03 = Console.ReadLine();
+            double preço1 = Convert.ToDouble(textoPreço01);
+            double preço2 = Convert.ToDouble(textoPreço02);
+            double preço3= Convert.ToDouble(textoPreço03);
+            double menor = 0;
+            if(preço1<preço2 &&preço1<preço3)
+            {
+                menor = preço1;
+                Console.WriteLine("o produto 1 é o menor");
+            }
+            else if (preço2<preço1 && preço2 < preço3 )
+            {
+                menor = preço2;
+                Console.WriteLine("o produto 2 é menor");
+            }
+            else
+            {
+                {
+                    menor = preço3;
+                    Console.WriteLine("o produto 3 é o menor ");
+                }
+                
+            }
+
+        }
+    }
+}
+
---
+++ Exercicio04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio04
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+Console.WriteLine("Digite o salário que será reajustado:");
+double salario = Convert.ToDouble(Console.ReadLine());            
+double percentualReajuste = 0;
+double valorAumento = 0;
+double novoSalario = 0;
+
+if (salario<2800)
+{
+    percentualReajuste = 0.2;
+}
+else if (salario>= 2800 &&salario<7000)
+{
+    percentualReajuste= 0.15;
+}
+else if (salario>= 7000 && salario<15000)
+{
+    percentualReajuste = 0.1;
+}
+else  
+{
+    percentualReajuste= 0.05;
+}
+valorAumento = percentualReajuste * salario;
+novoSalario = valorAumento+ salario;
+Console.WriteLine($"o salário original é{salario} . O percentual de reajuste é{percentualReajuste*100}. O valor real do aumento foi de {valorAumento}. o novo salário é :{novoSalario}  ");
+        }        
+    }
+}
